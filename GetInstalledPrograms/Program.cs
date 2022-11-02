a46a15f8d0af69fca4825a4a0d49f12317bf827f
Kirill <traineratwot@yandex.ru>
2022-11-02 17:56:37 +0300

    1

--- GetInstalledPrograms/Program.cs
+++ GetInstalledPrograms/Program.cs
@@ -25,14 +25,25 @@ namespace GetInstalledPrograms
     {
         public static void Main(string[] args)
         {
-            var registryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
-            using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registryKey))
+            var items = GetInstalledSoftwareList();
+            var options = new JsonSerializerOptions
             {
-                if (key == null) return;
-                var items = new List<Item>();
-                foreach (var subKeyName in key.GetSubKeyNames())
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+            Console.WriteLine(JsonSerializer.Serialize(items, options));
+        }
+
+        public static List<Item> GetInstalledSoftwareList()
+        {
+            string displayName;
+            var items = new List<Item>();
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", false))
+            {
+                foreach (String keyName in key.GetSubKeyNames())
                 {
-                    using (RegistryKey subKey = key.OpenSubKey(subKeyName))
+                    using (RegistryKey subKey = key.OpenSubKey(keyName))
                     {
                         if (subKey == null) continue;
                         if (subKey.GetValueNames().Length == 0) continue;
@@ -44,14 +55,66 @@ namespace GetInstalledPrograms
                             subKey.GetValue("InstallLocation")?.ToString()));
                     }
                 }
-
-                var options = new JsonSerializerOptions
-                {
-                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
-                    WriteIndented = true
-                };
-                Console.WriteLine(JsonSerializer.Serialize(items, options));
             }
+
+            //using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", false))
+            using (var localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                var key = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", false);
+                foreach (String keyName in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subKey = key.OpenSubKey(keyName))
+                    {
+                        if (subKey == null) continue;
+                        if (subKey.GetValueNames().Length == 0) continue;
+                        if (subKey.GetValue("SystemComponent") != null && subKey.GetValue("SystemComponent").ToString() == "1") continue;
+                        if (subKey.GetValue("InstallLocation") == null || subKey.GetValue("InstallLocation").ToString() == "") continue;
+                        items.Add(new Item(
+                            subKey.GetValue("DisplayName")?.ToString(),
+                            subKey.GetValue("Publisher")?.ToString(),
+                            subKey.GetValue("InstallLocation")?.ToString()));
+                    }
+                }
+            }
+
+            using (var localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
+            {
+                var key = localMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", false);
+                foreach (String keyName in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subKey = key.OpenSubKey(keyName))
+                    {
+                        if (subKey == null) continue;
+                        if (subKey.GetValueNames().Length == 0) continue;
+                        if (subKey.GetValue("SystemComponent") != null && subKey.GetValue("SystemComponent").ToString() == "1") continue;
+                        if (subKey.GetValue("InstallLocation") == null || subKey.GetValue("InstallLocation").ToString() == "") continue;
+                        items.Add(new Item(
+                            subKey.GetValue("DisplayName")?.ToString(),
+                            subKey.GetValue("Publisher")?.ToString(),
+                            subKey.GetValue("InstallLocation")?.ToString()));
+                    }
+                }
+            }
+
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall", false))
+            {
+                foreach (String keyName in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subKey = key.OpenSubKey(keyName))
+                    {
+                        if (subKey == null) continue;
+                        if (subKey.GetValueNames().Length == 0) continue;
+                        if (subKey.GetValue("SystemComponent") != null && subKey.GetValue("SystemComponent").ToString() == "1") continue;
+                        if (subKey.GetValue("InstallLocation") == null || subKey.GetValue("InstallLocation").ToString() == "") continue;
+                        items.Add(new Item(
+                            subKey.GetValue("DisplayName")?.ToString(),
+                            subKey.GetValue("Publisher")?.ToString(),
+                            subKey.GetValue("InstallLocation")?.ToString()));
+                    }
+                }
+            }
+
+            return items;
         }
     }
 }

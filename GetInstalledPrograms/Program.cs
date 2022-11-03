096d53a52b6fdc8da71a29954c21888d66a5900d
Kirill <traineratwot@yandex.ru>
2022-11-03 12:33:29 +0300

    1

--- GetInstalledPrograms/Program.cs
+++ GetInstalledPrograms/Program.cs
@@ -1,36 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Xml.Serialization;
 using Microsoft.Win32;
+
 namespace GetInstalledPrograms
 {
-	class Item
+    public class Program
     {
-        public string DisplayName { get; }
-        public string Publisher { get; }
-        public string InstallLocation { get; }
-
-        public Item(string displayName, string publisher, string installLocation)
+        public class Item
         {
-            DisplayName = displayName;
-            Publisher = publisher;
-            InstallLocation = installLocation;
-        }
-    }
+            [XmlAttribute]
+            public string DisplayName { get; set; }
+            [XmlAttribute]
+            public string Publisher { get; set; }
+            [XmlAttribute]
+            public string InstallLocation { get; set; }
 
-    internal class Program
-    {
+            public Item()
+            {
+            }
+        }
         public static void Main(string[] args)
         {
             using (var stringwriter = new System.IO.StringWriter())
             {
-                var items = GetInstalledSoftwareList();
-                var serializer = new XmlSerializer(items.GetType());
-                serializer.Serialize(stringwriter, items);
-                Console.WriteLine(stringwriter.ToString());
+                
+                var items = GetInstalledSoftwareList(); 
+                XmlSerializer xmlSerializer = new XmlSerializer(items.GetType());
+                using (var stream = new StringWriter())
+                {
+                    xmlSerializer.Serialize(stream, items);
+                    Console.WriteLine(stream);
+                }
+               
                 Environment.Exit(0);
             }
+
             Environment.Exit(1);
+        }
+
+        public static string ConvertObjectToXmlString(object classObject)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(classObject.GetType());
+            using (var stream = new StringWriter())
+            {
+                xmlSerializer.Serialize(stream, classObject);
+                return stream.ToString();
+            }
         }
 
         public static List<Item> GetInstalledSoftwareList()
@@ -47,13 +64,17 @@ namespace GetInstalledPrograms
                         if (subKey.GetValueNames().Length == 0) continue;
                         if (subKey.GetValue("SystemComponent") != null && subKey.GetValue("SystemComponent").ToString() == "1") continue;
                         if (subKey.GetValue("InstallLocation") == null || subKey.GetValue("InstallLocation").ToString() == "") continue;
-                        items.Add(new Item(
-                            subKey.GetValue("DisplayName")?.ToString(),
-                            subKey.GetValue("Publisher")?.ToString(),
-                            subKey.GetValue("InstallLocation")?.ToString()));
+                        var i = new Item
+                        {
+                            DisplayName = subKey.GetValue("DisplayName")?.ToString(),
+                            Publisher = subKey.GetValue("Publisher")?.ToString(),
+                            InstallLocation = subKey.GetValue("InstallLocation")?.ToString()
+                        };
+                        items.Add(i);
                     }
                 }
             }
+
             //using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", false))
             using (var localMachine = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
             {
@@ -66,10 +87,13 @@ namespace GetInstalledPrograms
                         if (subKey.GetValueNames().Length == 0) continue;
                         if (subKey.GetValue("SystemComponent") != null && subKey.GetValue("SystemComponent").ToString() == "1") continue;
                         if (subKey.GetValue("InstallLocation") == null || subKey.GetValue("InstallLocation").ToString() == "") continue;
-                        items.Add(new Item(
-                            subKey.GetValue("DisplayName")?.ToString(),
-                            subKey.GetValue("Publisher")?.ToString(),
-                            subKey.GetValue("InstallLocation")?.ToString()));
+                        var i = new Item
+                        {
+                            DisplayName = subKey.GetValue("DisplayName")?.ToString(),
+                            Publisher = subKey.GetValue("Publisher")?.ToString(),
+                            InstallLocation = subKey.GetValue("InstallLocation")?.ToString()
+                        };
+                        items.Add(i);
                     }
                 }
             }
@@ -85,10 +109,13 @@ namespace GetInstalledPrograms
                         if (subKey.GetValueNames().Length == 0) continue;
                         if (subKey.GetValue("SystemComponent") != null && subKey.GetValue("SystemComponent").ToString() == "1") continue;
                         if (subKey.GetValue("InstallLocation") == null || subKey.GetValue("InstallLocation").ToString() == "") continue;
-                        items.Add(new Item(
-                            subKey.GetValue("DisplayName")?.ToString(),
-                            subKey.GetValue("Publisher")?.ToString(),
-                            subKey.GetValue("InstallLocation")?.ToString()));
+                        var i = new Item
+                        {
+                            DisplayName = subKey.GetValue("DisplayName")?.ToString(),
+                            Publisher = subKey.GetValue("Publisher")?.ToString(),
+                            InstallLocation = subKey.GetValue("InstallLocation")?.ToString()
+                        };
+                        items.Add(i);
                     }
                 }
             }
@@ -103,10 +130,13 @@ namespace GetInstalledPrograms
                         if (subKey.GetValueNames().Length == 0) continue;
                         if (subKey.GetValue("SystemComponent") != null && subKey.GetValue("SystemComponent").ToString() == "1") continue;
                         if (subKey.GetValue("InstallLocation") == null || subKey.GetValue("InstallLocation").ToString() == "") continue;
-                        items.Add(new Item(
-                            subKey.GetValue("DisplayName")?.ToString(),
-                            subKey.GetValue("Publisher")?.ToString(),
-                            subKey.GetValue("InstallLocation")?.ToString()));
+                        var i = new Item
+                        {
+                            DisplayName = subKey.GetValue("DisplayName")?.ToString(),
+                            Publisher = subKey.GetValue("Publisher")?.ToString(),
+                            InstallLocation = subKey.GetValue("InstallLocation")?.ToString()
+                        };
+                        items.Add(i);
                     }
                 }
             }
